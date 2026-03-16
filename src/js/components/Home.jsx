631a24919793f62db559e7bd5b2efb2d082128bc
Alicia2202 <alicialf02@gmail.com>
2026-03-16 17:31:15 +0000

    Feat: Añadido botones de reset, resume y stop en home, y renderizo en main

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,27 +4,35 @@ import Digit from "./Digit";
 
 
 //create your first component
-const Home = ({ seconds }) => {
+const Home = ({ seconds, stop, resume, reset }) => {
 
 	const digits = (seconds.toString().padStart(6, "0")).split("")
-	
+
 	return (
-		<div className="container vh-100 d-flex  justify-content-center align-items-center">
-			<div className="p-2 iconoreloj">
-				<Digit num={<i className="fa-regular fa-clock"></i>} />
-			</div>
-			{
-				digits.map((digit,i) => {
+		<div className="container vh-100 d-flex flex-column justify-content-center align-items-center">
+			<div className="d-flex align-item-center">
+				<div className="p-2 iconoreloj">
+					<Digit num={<i className="fa-regular fa-clock"></i>} />
+				</div>
+				{digits.map((digit, i) => {
 					return (
-						<div  key={i} className="p-2">
+						<div key={i} className="p-2">
 							<Digit num={digit} />
 						</div>
 					)
 				})
-			}
-			
+				}
+			</div>
 
+
+			<div className="mt-3 d-flex gap-2">
+				<button onClick={stop} className="btn btn-danger">Stop</button>
+				<button onClick={resume} className="btn btn-success">Resume</button>
+				<button onClick={reset} className="btn btn-warning"><i className="fa-solid fa-arrow-rotate-left resetbutton"></i></button>
+			</div>
 		</div>
+
+
 	);
 };
 
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -12,15 +12,53 @@ import '../styles/index.css'
 import Home from './components/Home';
 
 let timer = 0
+let intervalId = null
 
-const root =ReactDOM.createRoot(document.getElementById('root'))
 
-setInterval(() => {
-  timer ++
-  timer == 1000000 ? timer = 0 : timer ++
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const stop = () => {
+  clearInterval(intervalId)
+  intervalId = null
+};
+
+const resume = () => {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      timer == 1000000 ? timer = 0 : timer ++
+      root.render(
+        <React.StrictMode>
+          <Home
+            seconds={timer}
+            stop={stop}
+            resume={resume}
+            reset={reset}
+          />
+        </React.StrictMode>
+      )
+    }, 1000)
+  }
+}
+
+const reset = () => {
+  clearInterval(intervalId)
+  intervalId = null
+  timer = 0
+
   root.render(
     <React.StrictMode>
-    <Home seconds={timer}/>
-  </React.StrictMode>,
-)
-},1000)
+      <Home
+        seconds={timer}
+        stop={stop}
+        resume={resume}
+        reset={reset}
+      />
+    </React.StrictMode>
+  )
+
+  resume()
+}
+
+resume()
+
+
